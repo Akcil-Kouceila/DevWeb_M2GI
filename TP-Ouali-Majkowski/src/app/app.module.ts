700585b3aeee3d4cb671900be0813e2c2534e66c
Akcil-Kouceila <akcil28@hotmail.fr>
2020-03-23 09:21:43 +0100

    Composants movies

--- TP-Ouali-Majkowski/src/app/app.module.ts
+++ TP-Ouali-Majkowski/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieComponent } from './pages/movie/movie.component';
+import { MoviesComponent } from './pages/movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    MoviesComponent,
   ],
   imports: [
     BrowserModule,
